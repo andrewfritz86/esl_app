72ea397d97b9c9d4ec377f9042d9bacda3a054a3
andrewfritz86 <andrew.fritz86@gmail.com>
2014-10-27 20:03:05 -0400

    fixes refresh bug

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -5,24 +5,17 @@ class StoriesController < ApplicationController
   end
 
   def template
-    #probably should create a new story here
-    #need to add logic to only create a new story when needed
-    #is there rails logic where we can see where we came from?
-    @story = Story.create
-    @story.user_id = session[:user_id]
-    @story.save
+    unless session[:story_id]
 
-    ##sessions below
-    # if params[:previous_story_id]
-    #   session[:story_id] = params[:previous_story_id]
-    #   #when someone wants to grab a previous story, that id will be sent over from the link_helper via params
-    #   #this only fires when someone clicks a link helper, because there won't be params otherwise
-    # end
+      @story = Story.create
+      @story.user_id = session[:user_id]
+      @story.save
 
-    if !session[:story_id] #&& !params[:previous_story_id]
-      session[:story_id] = @story.id
-      ##this is for new stories, however the logic is off, maybe should be an or statement
+      if !session[:story_id]
+        session[:story_id] = @story.id
+      end
     end
+
   end
 
   def index
